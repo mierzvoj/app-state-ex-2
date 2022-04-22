89a8f80d5c0af4469dafb7b6f55e8fb1716dcbf2
mierzvoj <mierzvoj@gmail.com>
2022-04-23 00:45:58 +0200

    first one

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
+import ReactDOM from "react-dom/client";
+
+
+const arr: number[] = [];
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const getRandom = () => Math.floor(Math.random() * 10);
+
+    for (let k = 0; k < 10; k++) {
+        arr.push(getRandom());
+
+    }
+
+    console.log(arr);
+
+    const listItems = arr.map((numbers, i) => (
+        <li key={`numbers -${i}`}> {numbers}</li>
+
+    ));
+
+
+    const [inputNumbers, setValue] = useState([listItems]);
+
+
+    function handleDelete(itemId: number) {
+        const items = [...state.inputNumbers];
+        var index = items.findIndex((i) => i.id === itemId);
+        if (index !== -1) {
+            items.splice(index, 1);
+            setValue({items});
+        }
+    }
+
+    // function deleteHandler = (itemId: number) => handleDelete(itemId);
+
+    return (
+        <ul>
+            {inputNumbers}
+        </ul>
+
+    );
 }
 
 export default App;
+
+
+const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+);
+root.render(<App/>);
